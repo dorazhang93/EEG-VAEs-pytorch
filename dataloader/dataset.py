1bfd2310fc5e97e47ed89e7f14e02bf5315c9f99
dorazhang93 <dorazhang93@gmail.com>
2023-03-11 13:59:54 +0100

    preprocess EEG and split train and vail. v1

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -1,57 +1,37 @@
+import os.path
+from .preprocess import Preprocesser
 import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset
 from typing import List, Optional, Sequence, Union, Any, Callable
 from pytorch_lightning import LightningDataModule
-from scipy import stats
+import scipy as sp, scipy.io
 
 
-class DNA(Dataset):
+class EEG(Dataset):
     def __init__(self,
                  data_dir: str,
                  split:str = "train",
                  transform: Callable =None,
-                 MFA: bool = False,
                  **kwargs):
         self.data_dir = data_dir
         self.transforms = transform
 
-        self.inds = np.load(self.data_dir+f"_inds_{split}.npy")
-        self.masks = np.load(self.data_dir+f"_mask_{split}.npy")
-        self.genotypes = np.load(self.data_dir+f"_genotype_{split}.npy")
-        print("@"*10+split+"@"*10)
-        print(f"Load data from {self.data_dir}")
-        print("genotype data shape:", self.genotypes.shape)
-        print("inds shape", self.inds.shape)
-        print("mask shape", self.masks.shape)
-        self.mf = None
-        if MFA:
-            self.mf = self._get_most_frequent()
-    def _get_most_frequent(self,):
-        print("get most frequent allele for each SNP")
-        geno = np.load(self.data_dir+"_genotype_all.npy")
-        msk = np.load(self.data_dir+"_mask_all.npy")
-        print("geno shape:" , geno.shape)
-        print("mask shape:", msk.shape)
-        mf = np.empty(geno.shape[1])
-        for i in range(geno.shape[1]):
-            mf_impute = geno[:,i][(1-msk[:,i]).astype(bool)]
-            if mf_impute.size>0:
-                mf[i]=mf_impute[0]
-            else:
-                mf[i]= stats.mode(geno[:,i]).mode[0]
-        return mf
-
+        self.recoding_meta = np.loadtxt(self.data_dir+f"/{split}.txt")
 
     def __len__(self):
-        assert len(self.genotypes)==len(self.inds)
-        assert len(self.genotypes)==len(self.masks)
-        return len(self.genotypes)
+        return len(self.recoding_meta)
 
     def __getitem__(self, idx):
-        input = self.genotypes[idx]
-        # input shape (L)
-        return torch.from_numpy(input)
+        recoding_file, quality_score = self.recoding_meta[idx]
+        # load patient meata to get target values like cpc
+        eeg = np.asarray(sp.io.loadmat(self.data_dir+"/"+recoding_file+".mat")["val"])
+        header = np.genfromtxt("",dtype=str,skip_header=True,usecols=(2,4))
+        gain = np.array([float(x.split('/')[0]) for x in header[:,]])
+        offset = header[:,1].astype(float)
+        eeg = self._rescale(eeg, gain, offset)
+        eeg = self._crop()
+        return torch.from_numpy(eeg)
 
 class VAEDataset(LightningDataModule):
     def __init__(self,
@@ -71,28 +51,32 @@ class VAEDataset(LightningDataModule):
         self.num_workers = num_workers
         self.pin_memory = pin_memory
     def prepare_data(self):
+        train_split_file = self.data_dir + "/" +self.data_name +"train.txt"
+        val_split_file = self.data_dir + "/" +self.data_name +"val.txt"
+        if (not os.path.isfile(train_split_file)) or (not os.path.isfile(val_split_file)):
+            pre = Preprocesser(self.data_dir,self.data_name)
+            pre._train_val_split()
         pass
     def setup(self,stage=None):
         if stage=="predict":
-            self.all_dataset = DNA(
+            self.all_dataset = EEG(
             self.data_dir + self.data_name,
             split="all",
             )
             return
         elif stage=="fit":
-            self.train_dataset = DNA(
+            self.train_dataset = EEG(
             self.data_dir+self.data_name,
             split="train",
             )
-            self.val_dataset = DNA(
+            self.val_dataset = EEG(
             self.data_dir + self.data_name,
             split="val",
             )
         elif stage=="test":
-            self.val_dataset = DNA(
+            self.val_dataset = EEG(
             self.data_dir + self.data_name,
             split="val",
-            MFA=True,
             )
         else:
             raise NotImplementedError
--- dataloader/preprocess.py
+++ dataloader/preprocess.py
@@ -1,169 +1,143 @@
+import sys
+import scipy as sp
 import numpy as np
-from scipy import stats
 from sklearn.model_selection import train_test_split
-from visualizer.plots import plot_genotype_hist
-
+import os
+from dataloader.helper import find_data_folders, load_text_file, get_recording_ids, get_quality_scores
 rng = np.random.RandomState(0)
 
-def g(file):
-    with open(file,"r") as f:
-        for line in f.readlines():
-            for item in list(map(int, line.strip("\n"))):
-                yield item
+def _check(header_file, recording_file):
+    # Load the header file.
+    if (not os.path.isfile(header_file)) or (not os.path.isfile(recording_file)):
+        print(f"header file {header_file} or recorfing file {recording_file} doesn't exist!")
+        return False
+
+    with open(header_file, 'r') as f:
+        header = [l.strip() for l in f.readlines() if l.strip()]
+
+    # Parse the header file.
+    record_name = None
+    num_signals = None
+    sampling_frequency = None
+    num_samples = None
+    signal_files = list()
+    gains = list()
+    offsets = list()
+    channels = list()
+    initial_values = list()
+    checksums = list()
+
+    for i, l in enumerate(header):
+        arrs = [arr.strip() for arr in l.split(' ')]
+        # Parse the record line.
+        if i==0:
+            record_name = arrs[0]
+            num_signals = int(arrs[1])
+            sampling_frequency = float(arrs[2])
+            num_samples = int(arrs[3])
+        # Parse the signal specification lines.
+        else:
+            signal_file = arrs[0]
+            gain = float(arrs[2].split('/')[0])
+            offset = int(arrs[4])
+            initial_value = int(arrs[5])
+            checksum = int(arrs[6])
+            channel = arrs[8]
+            signal_files.append(signal_file)
+            gains.append(gain)
+            offsets.append(offset)
+            initial_values.append(initial_value)
+            checksums.append(checksum)
+            channels.append(channel)
+
+    # Check that the header file only references one signal file. WFDB format  allows for multiple signal files, but we have not
+    # implemented that here for simplicity.
+    num_signal_files = len(set(signal_files))
+    if num_signal_files!=1:
+        print('The header file {}'.format(header_file) \
+            + ' references {} signal files; one signal file expected.'.format(num_signal_files))
+        return False
+
+    # Load the signal file.
+    head, tail = os.path.split(header_file)
+    signal_file = os.path.join(head, list(signal_files)[0])
+    data = np.asarray(sp.io.loadmat(signal_file)['val'])
+
+    # Check that the dimensions of the signal data in the signal file is consistent with the dimensions for the signal data given
+    # in the header file.
+    num_channels = len(channels)
+    if np.shape(data)!=(num_channels, num_samples):
+        print('The header file {}'.format(header_file) \
+            + ' is inconsistent with the dimensions of the signal file.')
+        return  False
+    # Check that the initial value and checksums for the signal data in the signal file are consistent with the initial value and
+    # checksums for the signal data given in the header file.
+    for i in range(num_channels):
+        if data[i, 0]!=initial_values[i]:
+            print('The initial value in header file {}'.format(header_file) \
+                + ' is inconsistent with the initial value for channel'.format(channels[i]))
+            return False
+        if np.sum(data[i, :])!=checksums[i]:
+            print('The checksum in header file {}'.format(header_file) \
+                + ' is inconsistent with the initial value for channel'.format(channels[i]))
+            return False
+    return True
+
+
 
 class Preprocesser:
     def __init__(self,
-                 filebase: str,
-                 normalization: bool = True,
-                 impute_missing: str = "MF",
+                 root: str,
+                 folder: str,
                  train_val_split: float = 0.2,
-                 missing_val_ori: float = 9.0,
-                 missing_val_norm: float = -1.0,
-                 split_policy: str = "random"):
-        """
-        @param filebase: path+ filename without suffix eg. "/home/data/TPS_full/new_TPS"
-        @param normalization: wheter to normalize input
-        @param impute_missing: method for missing value imputation, no imputation if None
-        @param train_val_split: fraction of validation set
-        @param missing_val_ori: missing value in input
-        @param missing_val_norm: missing value after normalization
-        """
-        self.filebase = filebase
-        self.normalization = normalization
-        self.impute_missing = impute_missing
+                 ):
+        self.root = root
+        self.folder = folder
+        self.patient_folders = find_data_folders(root+"/"+folder)
         self.split = train_val_split
-        self.missing_val_ori = missing_val_ori
-        self.missing_val_norm = missing_val_norm
-        self.split_policy = split_policy
-        self.inds, self.targets = self._load_inds()
-        self.genotypes = self._load_genotype()
-        self.missing_mask = (self.genotypes!=self.missing_val_ori).astype(int).T # 0 means missing
-        print(f"Overall missing rate is {1-self.missing_mask.sum()/self.missing_mask.size}")
+        self.eeg_segments_list = self._find_segments()
+        self.num_patients = len(self.patient_folders)
+        self._check_eegs()
 
-    def _load_inds(self):
-        """
-        @return: individual and corresponding population
-        @rtype: np.array in shape of (num_inds, 2), col1 is ID, col2 is population
-        """
-        ind_pop_list = np.genfromtxt(self.filebase + ".ind", usecols=(0, 2), dtype=str)
-        print("Reading individual profiles from " + self.filebase + ".ind")
-        targets = np.genfromtxt(self.filebase + ".ind", usecols=(3,4,5,6), dtype=float)
-        print("Reading targets from " + self.filebase + ".ind")
-        return ind_pop_list, targets
+    def _find_segments(self):
+        eegs_file_lists = list()
+        for p_folder in self.patient_folders:
+            recording_meta_file = os.path.join(self.root,self.folder,p_folder,p_folder+".tsv")
+            recording_meta = load_text_file(recording_meta_file)
+            for quality_score, recording_id in zip(get_quality_scores(recording_meta), get_recording_ids(recording_meta)):
+                if recording_id == "nan":
+                    continue
+                if quality_score == "nan":
+                    print(quality_score, recording_id)
+                    continue
+                recording_file = os.path.join(p_folder,recording_id)
+                eegs_file_lists.append([recording_file,quality_score])
+        return eegs_file_lists
 
-    def _load_genotype(self):
-        """
-        @return: raw genotype data in 0,1,2,9(missing)
-        @rtype: np.array in shape of (num_SNPs, num_inds)
-        """
-        num_inds = len(self.inds)
-        genotypes = np.fromiter(g(self.filebase + ".eigenstratgeno"), dtype=float).reshape(-1, num_inds)
-        print("Reading genotypes from " + self.filebase + ".eigenstratgeno")
-        print("genotypes's shape", genotypes.shape)
-        return genotypes
-
-    def _normalize(self):
-        """
-        Normalize genotypes into interval [0,1] by translating 0,1,2 -> 0.0, 0.5, 1.0, missing value (default 9) -> -1
-        """
-        self.genotypes[self.genotypes == 1.0] = 0.5
-        self.genotypes[self.genotypes == 2.0] = 1.0
-        self.genotypes[self.genotypes == self.missing_val_ori] = self.missing_val_norm
-
-    def _impute_missing(self):
-        if self.impute_missing == "MF":
-            self._most_frequent_impute()
-            print("Imputed missing value using most frequent allele")
-        elif self.impute_missing == "AE":
-            self._AE_impute()
-            print("Imputed missing value using AE reconstruction")
-        else:
-            raise NotImplementedError(f"{self.impute_missing} is not implemented")
-
-    def _most_frequent_impute(self):
-        for m in self.genotypes:
-            # exclude missing value before count modes
-            modes = stats.mode(m[m!=self.missing_val_norm])
-            most_frequent = modes.mode
-            if most_frequent.size ==0:
-                raise Warning("Run into SNPs with 100% missing rate")
-            else:
-                m[m==self.missing_val_norm]=most_frequent[0]
-
-    def _AE_impute(self):
-        # Reconstructed genotype file should be placed at the same folder as .eigenstratgeno file
-        # and with suffix .npy
-        reconstruct= np.load(self.filebase+".npy", dtype=float)
-        print ("load reconstructed genotype from "+ self.filebase+".npy")
-        assert reconstruct.shape == self.genotypes
-        # normalize before autoencoder imputation
-        if not self.normalization:
-            print("Normalization before AE")
-            self._normalize()
-        self.genotypes = self.genotypes * self.missing_mask + reconstruct * (1-self.missing_mask)
+    def _check_eegs(self):
+        segments_checked = list()
+        for eeg_segment in self.eeg_segments_list:
+            eeg_segment_file, _ = eeg_segment
+            header_file = self.root + "/" + self.folder + "/" + eeg_segment_file +".hea"
+            recording_file = self.root + "/" + self.folder + "/" + eeg_segment_file +".mat"
+            if _check(header_file, recording_file):
+                segments_checked.append(eeg_segment)
+        self.eeg_segments_list = segments_checked
 
 
     def _train_val_split(self):
-        if self.split_policy == "random":
-            geno_train, geno_val, inds_train, inds_val, mask_train, mask_val= train_test_split(self.genotypes, self.inds,
-                                                                                               self.missing_mask,
-                                                                                               test_size=self.split,
-                                                                                               random_state=rng)
-        elif self.split_policy == "stratified":
-            pop_list=self.inds[:,1]
-            pops, pop_counts = np.unique(pop_list,return_counts=True)
-            rare_pops = pops[pop_counts==1]
-            for rare in rare_pops:
-                pop_list[pop_list==rare]="Rare_pop"
+        eeg_train, eeg_val = train_test_split(self.eeg_segments_list,test_size=self.split)
+        np.savetxt(self.root+"/"+self.folder+"/all.txt", np.array(self.eeg_segments_list),fmt=str)
+        np.savetxt(self.root+"/"+self.folder+"/train.txt", np.array(eeg_train),fmt= str)
+        np.savetxt(self.root+"/"+self.folder+"/val.txt", np.array(eeg_val),fmt=str)
 
-            geno_train, geno_val, inds_train, inds_val, mask_train, mask_val = train_test_split(self.genotypes, self.inds,
-                                                                                                self.missing_mask,
-                                                                                                test_size=self.split,
-                                                                                                random_state=rng,
-                                                                                                stratify=pop_list)
-        else:
-            raise NotImplementedError(f"{self.split_policy} is not implemented" )
-        return geno_train, geno_val, inds_train, inds_val, mask_train, mask_val
-
-    def _save(self):
-        return
-
-    def process(self):
-        """
-        input should be raw genotype data in 0,1,2,9(missing)
-        """
-        if self.normalization:
-            self._normalize()
-            print("Normalized genome by mapping 0 to 0, 1 to 0.5, 2 to 1.0")
-        if self.impute_missing is not None:
-            self._impute_missing()
-        #     transpose genotype data into shape of (n_inds, n_SNPs)
-        self.genotypes = self.genotypes.T
-        plot_genotype_hist(self.genotypes,self.filebase+"_genotype_hist")
-        geno_train, geno_val, inds_train, inds_val, mask_train, mask_val = self._train_val_split()
-        # save data
-        data_dict = {"genotype":{"all":self.genotypes,"train":geno_train,"val":geno_val},
-                     "inds":{"all":self.inds,"train":inds_train,"val":inds_val},
-                     "mask":{"all":self.missing_mask,"train":mask_train,"val":mask_val}}
-        for dtyp , v in data_dict.items():
-            for split, value in v.items():
-                if dtyp == "genotype":
-                    np.save(self.filebase+f"_{self.impute_missing}_{dtyp}_{split}.npy",value.astype(np.float16))
-                elif dtyp == "inds":
-                    np.save(self.filebase+f"_{self.impute_missing}_{dtyp}_{split}.npy",value)
-                elif dtyp =="mask":
-                    np.save(self.filebase+f"_{self.impute_missing}_{dtyp}_{split}.npy",value.astype(int))
-                else:
-                    raise ValueError(f"Wrong data {dtyp}")
-        np.save(self.filebase+f"_{self.impute_missing}_Y_all.npy", self.targets)
 
 
 if __name__ == "__main__":
-    params= {"filebase":"/home/etlar/daqu/Projects/genome/data/aDNA/new_data_1Feb_Daqu/TPS2_slim/TPS2_mind5_ex_2233modern_maf05",
-             "normalization": True,
-             "impute_missing": "MF",
-             "train_val_split": 0.2,
-             "missing_val_ori": 9.0,
-             "split_policy": "stratified"}
-    processer = Preprocesser(**params)
-    processer.process()
+    data_dir="/home/daqu/Projects/Physionet/physionet.org/files/i-care/1.0"
+    data_name="training"
+    train_split_file = data_dir + "/" + data_name + "train.txt"
+    val_split_file = data_dir + "/" + data_name + "val.txt"
+    if (not os.path.isfile(train_split_file)) or (not os.path.isfile(val_split_file)):
+        pre = Preprocesser(data_dir, data_name)
+        pre._train_val_split()
